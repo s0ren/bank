ebf44d064631bbbd45157943aed40c1e907d7466
New123User321 <Ny123Mail321@gmail.com>
2017-04-20 11:56:37 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 var holders = function()
 {
-	var holder = []
+	var holder = [];
 	
 	this.add = function(input)
 	{
@@ -9,37 +9,14 @@ var holders = function()
 	
 	this.show = function()
 	{
-		return(holder)
+		
+		return(holder.slice(0))
 	}
 }
 
-Transaktioner = new holders()
+Transaktioner = new holders();
 
-/*
-var Account = function(id, owner)
-{
-	var id = id;
-	var owner = owner;
-	var balance = 10000;
-	
-	this.transfer = function(toId, amount)
-	{
-		//hvis findes id
-		if(amount > 0 && balance >= amount)
-		{
-			balance = balance - amount
-			Transaktioner.add({"from": id, "to": toId, "amount": amount})
-		}
-		else{throw("Error")}
-	}
-	this.getBalance = function()
-	{
-		return(balance)
-	}
-}
-**/
-
-var konto = function(id, saldo, ejer, rentefod)
+var Konto = function(id, saldo, ejer, rentefod)
 {
 	if(typeof(id) == "number", typeof(saldo) == "number", typeof(ejer) == "string")
 	{
@@ -58,11 +35,101 @@ var konto = function(id, saldo, ejer, rentefod)
 				{
 					return(saldo)
 				}
-				//"set": function() {}
+//				"set": function(tilId, belob)
+//				{
+//					if(typeof(TilId) == "number" && typeof(belob) == "number")
+//					{
+//						if(saldo >= belob)
+//						{
+//							saldo = saldo - belob
+//							Transaktioner.add({"fra":id, "til": tilId, "belob": belob})
+//						}
+//						else{throw("ikke nok på saldoen")}
+//					}
+//					else{throw("fejl ved id'et eller beløbet")}
+//				}
+			}
+		);
+		Object.defineProperty
+		(
+			that, "id",
+			{
+				"get":function()
+				{
+					return(id)
+				}
 			}
 		)
+		
+		that.udtraek = function(belob)
+		{
+			if(typeof(belob) == "number" && that.validerUdtraek(belob))
+			{
+				saldo = saldo - belob;
+			}
+		}
+		
+		that.indsaet = function(belob)
+		{
+			if(typeof(belob) == "number" && that.validerIndaet(belob))
+			{
+				saldo = saldo + belob
+			}
+		}
+		
+		that.validerUdtraek = function(belob)
+		{
+			if(saldo >= belob)
+			{
+				return(true)
+			}
+			else{return(false)}
+		}
+		
+		that.validerIndaet = function()
+		{
+			return(true)
+		}
 	}
 	else{throw("Fejl i enten id'et eller saldo'en eller ejer")}
 	
 	return(that)
+}
+
+var Transaktion = function(fraKonto, tilKonto, belob, teller)
+{
+	if(typeof(fraKonto) == "object" && typeof(tilKonto) == "object" && typeof(belob) == "number" && typeof(teller) == "object")
+	{
+		var that = {}
+	
+		var fraKonto = fraKonto
+		var tilKonto = tilKonto
+		var belob = belob
+		var teller = teller
+		
+		var erBrugt = false
+		
+		that.udfor = function()
+		{
+			if(that.valider() == true && erBrugt == false)
+			{
+				fraKonto.udtraek(belob)
+				tilKonto.indsaet(belob)
+				Transaktioner.add({"fra": fraKonto.id, "til": tilKonto.id, "belob": belob, "teller": teller})
+				erBrugt = true
+				return("overførslen gik igemmen")
+			}
+			else{return("Kan ikke valider overførslen")}
+		}
+		that.valider = function()
+		{
+			if(fraKonto.validerUdtraek(belob) == true && tilKonto.validerIndaet(belob) == true)
+			{
+				return(true)
+			}
+			else{return(false)}
+		}
+		return(that)
+	}
+	else{throw("Fejl ved fra konten, til konten, beløbet eller telleren")}
 }
